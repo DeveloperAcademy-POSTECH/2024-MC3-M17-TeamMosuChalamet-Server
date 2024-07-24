644e2c6edfcceb67d2262fba16564eef9d897a0a
Dobbinci <davinlsj@gmail.com>
2024-07-24 21:38:46 +0900

    feat: MemberRepository 구현

--- src/main/kotlin/com/be/shoackserver/domain/repository/MemberRepository.kt
+++ src/main/kotlin/com/be/shoackserver/domain/repository/MemberRepository.kt
@@ -2,6 +2,8 @@ package com.be.shoackserver.domain.repository
 
 import com.be.shoackserver.domain.entity.Member
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface MemberRepository : JpaRepository<Member, Long>{
 }
