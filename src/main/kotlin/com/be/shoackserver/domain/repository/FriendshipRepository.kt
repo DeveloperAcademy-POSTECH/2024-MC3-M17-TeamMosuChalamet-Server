ab6262643beafa1e4ad53177184f0c4082d966c1
Dobbinci <davinlsj@gmail.com>
2024-07-25 11:38:45 +0900

    feat: FriendRepository 구현

--- src/main/kotlin/com/be/shoackserver/domain/repository/FriendshipRepository.kt
+++ src/main/kotlin/com/be/shoackserver/domain/repository/FriendshipRepository.kt
@@ -1,7 +1,9 @@
 package com.be.shoackserver.domain.repository
 
+import com.be.shoackserver.domain.entity.Friendship
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface FriendshipRepository {
+interface FriendshipRepository : JpaRepository<Friendship, Long>{
 }
