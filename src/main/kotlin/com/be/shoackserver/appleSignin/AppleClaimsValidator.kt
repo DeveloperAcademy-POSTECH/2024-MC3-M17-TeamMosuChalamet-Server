1c07933c6b3f0910df36e6b0b7ebe33aff2df644
Dobbinci <davinlsj@gmail.com>
2024-07-29 15:28:11 +0900

    Feat: <Merge> main -> apple-signin

--- src/main/kotlin/com/be/shoackserver/appleSignin/AppleClaimsValidator.kt
+++ src/main/kotlin/com/be/shoackserver/appleSignin/AppleClaimsValidator.kt
@@ -1,17 +1,17 @@
-package com.be.shoackserver.appleSignin
-
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.stereotype.Component
-
-@Component
-class AppleClaimsValidator(
-    @Value("\${oauth.apple.iss}") private val iss: String,
-    @Value("\${oauth.apple.cliendt-id}") private val clientId: String,
-    @Value("\${oauth.apple.nonce}") private val nonce: String,
-) {
-    private val encryptedNonce: String = EncryptUtils.encrypt(nonce)
-
-    companion object {
-        private const val NONCE_KEY = "nonce"
-    }
-}
+//package com.be.shoackserver.appleSignin
+//
+//import org.springframework.beans.factory.annotation.Value
+//import org.springframework.stereotype.Component
+//
+//@Component
+//class AppleClaimsValidator(
+//    @Value("\${oauth.apple.iss}") private val iss: String,
+//    @Value("\${oauth.apple.cliendt-id}") private val clientId: String,
+//    @Value("\${oauth.apple.nonce}") private val nonce: String,
+//) {
+//    private val encryptedNonce: String = EncryptUtils.encrypt(nonce)
+//
+//    companion object {
+//        private const val NONCE_KEY = "nonce"
+//    }
+//}
